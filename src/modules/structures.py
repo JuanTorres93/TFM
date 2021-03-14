57c458e9e945271e44a38fc5d943f8e9ab9f80b6
juan <JuanTorresNavarro93@gmail.com>
2021-03-14 20:58:40 +0100

    Added some methods to Node a Bar classes and some TODOs

--- src/modules/structures.py
+++ src/modules/structures.py
@@ -1,6 +1,9 @@
-class Node():
-    # TODO add support class or enum
-    def __init__(self, name, x, y, z, force=(0, 0, 0), momentum=(0, 0, 0), support):
+# TODO improve all calculations using numpy
+# TODO add support class or enum
+
+class Node:
+    # TODO CHANGE SUPPORT BY ITS TYPE AND ADD A SETTER METHOD
+    def __init__(self, name: str, x: float, y: float, z: float, force=(0, 0, 0), momentum=(0, 0, 0), support=0):
         self.name = name
         self.x = x
         self.y = y
@@ -9,14 +12,53 @@ class Node():
         self.momentum = momentum
         self.support = support
 
+    # TODO Write tests for the methods below
+    def set_name(self, new_name):
+        self.name = new_name
 
-class Bar():
-    def __init__(self, name, origin, end):
-        self.name
-        self.origin
-        self.end
+    def set_force(self, new_force):
+        self.force = new_force
+
+    def set_momentum(self, new_momentum):
+        self.momentum = new_momentum
+
+    def set_x(self, new_x):
+        self.x = new_x
+
+    def set_y(self, new_y):
+        self.y = new_y
+
+    def set_z(self, new_z):
+        self.z = new_z
+
+    def set_position(self, new_position):
+        self.x = new_position.x
+        self.y = new_position.y
+        self.z = new_position.z
 
 
-class Structure():
-    def __init__(self, bars):
+class Bar:
+    def __init__(self, name: str, origin, end):
+        # origin: Node
+        # bar: Node
+
+        self.name = name
+        self.origin = origin
+        self.end = end
+
+    # TODO Write tests for the methods below
+    def set_name(self, new_name):
+        self.name = new_name
+
+    def set_origin(self, new_origin):
+        self.origin = new_origin
+
+    def set_end(self, new_end):
+        self.end = new_end
+
+
+class Structure:
+    # TODO When creating a structure check that there aren't multiple bars with the same name
+    def __init__(self, name, bars):
+        # TODO bars will be a list, or maybe better, a ditionary of bars
         self.bars = bars
