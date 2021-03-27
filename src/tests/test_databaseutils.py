70bb2a7275b34799c93c13df855ec3760caea976
juan <JuanTorresNavarro93@gmail.com>
2021-03-27 18:59:52 +0100

    Expanded test for new columns in material table

--- src/tests/test_databaseutils.py
+++ src/tests/test_databaseutils.py
@@ -9,15 +9,18 @@ except ModuleNotFoundError:
 from src.modules import databaseutils as db
 
 
-
 class TestQuerys(unittest.TestCase):
-    # TODO Modify support when it is defined
     def test_add_material_to_db(self):
-
         # TODO add tuple size assert
-        self.assertRaises(TypeError, db.add_material_to_db, (9, "foo", 9))
-        self.assertRaises(TypeError, db.add_material_to_db, ("foo", 2, 9))
-        self.assertRaises(TypeError, db.add_material_to_db, ("foo", "bar", "r"))
+        self.assertRaises(ValueError, db.add_material_to_db, ("foo", "bar", 9))
+        self.assertRaises(TypeError, db.add_material_to_db, (9, "foo", 9, 8, 8, 8, 8))
+        self.assertRaises(TypeError, db.add_material_to_db, ("foo", 2, 9, 8, 8, 8, 8))
+        self.assertRaises(TypeError, db.add_material_to_db, ("foo", "bar", "r", 8, 8, 8, 8))
+        self.assertRaises(TypeError, db.add_material_to_db, ("foo", "bar", 8, "2", 8, 8, 8))
+        self.assertRaises(TypeError, db.add_material_to_db, ("foo", "bar", 8, 2, "3", 8, 8))
+        self.assertRaises(TypeError, db.add_material_to_db, ("foo", "bar", 8, 2, 4.9, "4", 8))
+        self.assertRaises(TypeError, db.add_material_to_db, ("foo", "bar", 8, 2, 4.9, 5.7, "3"))
+
 
 if __name__ == '__main__':
     unittest.main()
