67d0ec0b4337570dbfb051ee8d0b8edbe4279b2f
juan <JuanTorresNavarro93@gmail.com>
2021-03-27 18:57:58 +0100

    Added new table profile to database

--- src/modules/databaseutils.py
+++ src/modules/databaseutils.py
@@ -55,31 +55,51 @@ def regenerate_initial_database(force=False):
 
         connection = create_connection()
 
-        queries = []
-
-        create_materials_table = """
+        queries = [
+        # https://www.codigotecnico.org/pdf/Documentos/SE/DBSE-A.pdf
+        """
         CREATE TABLE IF NOT EXISTS materials (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           generic_name TEXT NOT NULL,
           name TEXT NOT NULL UNIQUE,
-          e REAL NOT NULL,
-          g REAL NOT NULL,
-          v REAL NOT NULL,
-          a REAL NOT NULL,
-          d REAL NOT NULL
+          young_mod REAL NOT NULL, -- Young's modulus
+          rig_mod REAL NOT NULL, -- rigidity modulus
+          poisson_coef REAL NOT NULL, -- Poisson coeficiente
+          thermal_dil_coef REAL NOT NULL, -- thermal dilatation
+          density REAL NOT NULL  -- density
         );
+        """,
+        # More fields in http://prontuarios.info/perfiles/IPE
         """
-        # TODO ser más exhaustivo con los datos de g, v, a, d
-        populate_materials_table = """
-        INSERT INTO materials (generic_name, name, e, g, v, a, d)
+        CREATE TABLE IF NOT EXISTS profiles (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL UNIQUE, -- IPE, HEB...
+        name_number INTEGER NOT NULL, -- e.g. 300 in IPE 300
+        area REAL NOT NULL, 
+        weight REAL NOT NULL, -- kg/m
+        inertia_moment_x REAL NOT NULL,
+        res_mod_x REAL NOT NULL,
+        inertia_moment_y REAL NOT NULL,
+        res_mod_y REAL NOT NULL
+        );
+        """,
+        # TODO ser más exhaustivo con los datos de g, v, a, d (¿Son EXACTAMENTE iguales para TODOS los aceros?)
+        """
+        INSERT INTO materials (generic_name, name, young_mod, rig_mod, poisson_coef, 
+        thermal_dil_coef, density)
         VALUES ('steel', 's275j', 205939650000, 81000000000, 0.3, 0.000012, 7.85);
+        """,
         """
-
-        queries.append(create_materials_table)
-        queries.append(populate_materials_table)
+        INSERT INTO profiles (name, name_number, area, weight, inertia_moment_x, 
+        res_mod_x, inertia_moment_y, res_mod_y)
+        VALUES ('IPE', 80, 0.000764, 6, 0.000000801, 0.000020, 0.0000000849,
+        0.00000369);
+        """
+        ]
 
         for query in queries:
             execute_query(connection, query)
+
 
 def add_material_to_db(material_info):
     """
@@ -87,6 +107,8 @@ def add_material_to_db(material_info):
     :param material_info: Tuple with the values in the following order (generic_name, name, e)
     :return:
     """
+    if len(material_info) < 7:
+        raise ValueError("Not enough information was provided.")
     if type(material_info[0]) not in [str] or type(material_info[1]) not in [str]:
         raise TypeError("material_info[0] and material_info[1] must be str")
     # Maybe the below condition will have to be updated when using numpy
@@ -100,12 +122,12 @@ def add_material_to_db(material_info):
     conn = create_connection()
     query = """
     INSERT INTO materials (generic_name, name, e, g, v, a, d)
-    VALUES (""" + "'" + material_info[0] + "', '" + material_info[1] + "', " + str(material_info[2]) + ");";
+    VALUES (""" + "'" + material_info[0] + "', '" + material_info[1] + "', " + str(material_info[2]) + ");"
 
     execute_query(conn, query)
 
 
-# regenerate_initial_database(force=True)
-# conn = create_connection()
-# x = execute_read_query(conn, "SELECT * FROM materials;")
-# print(x)
+regenerate_initial_database(force=True)
+conn = create_connection()
+x = execute_read_query(conn, "SELECT * FROM materials;")
+print(x)
