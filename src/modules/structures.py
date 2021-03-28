50cd3f44e4023e58e50507b907851ffbb5f324c2
juan <JuanTorresNavarro93@gmail.com>
2021-03-28 18:57:57 +0200

    Changed some functions to use numpy. Implemented method for global rigidity matrix

--- src/modules/structures.py
+++ src/modules/structures.py
@@ -1,4 +1,3 @@
-# TODO improve all calculations using numpy
 # TODO add support class or enum ¿tuple?
 import math
 import numpy as np
@@ -13,22 +12,22 @@ class Node:
             raise TypeError("name must be of type str")
 
         self.name = name
-        self.position = position
-        self.force = force
-        self.momentum = momentum
+        self.position = np.array(position)
+        self.force = np.array(force)
+        self.momentum = np.array(momentum)
         self.support = support
 
     def set_name(self, new_name):
         self.name = new_name
 
     def set_force(self, new_force):
-        self.force = new_force
+        self.force = np.array(new_force)
 
     def set_momentum(self, new_momentum):
-        self.momentum = new_momentum
+        self.momentum = np.array(new_momentum)
 
     def set_position(self, new_position):
-        self.position = new_position
+        self.position = np.array(new_position)
 
     def x(self):
         return self.position[0]
@@ -41,7 +40,6 @@ class Node:
 
 
 class Bar:
-    # TODO Add material and section
     def __init__(self, name: str, origin, end, material="s275j", profile=("IPE", 300)):
         # origin: Node
         # bar: Node
@@ -60,7 +58,6 @@ class Bar:
         self.material = Material(material)
         self.profile = Profile(profile[0], profile[1])
 
-    # TODO Write tests for the methods below
     def set_name(self, new_name):
         self.name = new_name
 
@@ -71,9 +68,7 @@ class Bar:
         self.end = new_end
 
     def length(self) -> float:
-        return math.sqrt((self.end.x() - self.origin.x()) ** 2 +
-                         (self.end.y() - self.origin.y()) ** 2 +
-                         (self.end.z() - self.origin.z()) ** 2)
+        return np.linalg.norm(np.subtract(self.end.position, self.origin.position))
 
     def set_material(self, mat_name):
         """
@@ -92,16 +87,23 @@ class Bar:
         # TODO Add more profiles to database and write a test for this function
         self.profile = Profile(profile_name, profile_number)
 
-    def local_rigidity_matrix_2d_rigid_nodes(self, i):
-        # TODO Possible improvement, get e, a and i from material and section properties
-        # e -> Young's modulus
-        # a -> cross section's area
-        # l -> beam length
-        # i -> modulus inertia
+    def local_rigidity_matrix_2d_rigid_nodes(self, use_inertia_x=True):
+        """
 
-        l = self.length()
-        e = self.material.young_mod
-        a = self.profile.area
+        :param use_inertia_x: Specifies whether the inertia moment must be selected with respect to the
+        x axis (default) or the y axis (f equals False)
+        :return: local rigidity matrix for a 2D structure with rigid nodes
+        """
+        # TODO Check when it must be used inertia momentum x or y
+
+        l = self.length()  # l -> beam length
+        e = self.material.young_mod  # e -> Young's modulus
+        a = self.profile.area  # a -> cross section's area
+        # i -> modulus inertia
+        if use_inertia_x:
+            i = self.profile.inertia_moment_x
+        else:
+            i = self.profile.inertia_moment_y
 
         return np.array([[e * a / l, 0, 0, -e * a / l, 0, 0],
                          [0, 12 * e * i / l ** 3, 6 * e * i / l ** 2, 0, -12 * e * i / l ** 3, 6 * e * i / l ** 2],
@@ -110,9 +112,26 @@ class Bar:
                          [0, -12 * e * i / l ** 3, -6 * e * i / l ** 2, 0, 12 * e * i / l ** 3, -6 * e * i / l ** 2],
                          [0, 6 * e * i / l ** 2, 2 * e * i / l, 0, -6 * e * i / l ** 2, 4 * e * i / l]])
 
-    def system_change_matrix_2d_rigid_nodes(self, angle):
-        """angle represents the angle that the global axis must be rotated in order to be the same
-        than the local ones. angle is assumed in radians"""
+    def _angle_from_global_to_local(self):
+        """
+
+        :return: Needed angle for converting the local matrix to global one
+        """
+        reference = np.array((1, 0, 0))
+        beam_line = np.subtract(self.end.position, self.origin.position)
+
+        dot_product = reference @ beam_line
+
+        cosine = dot_product / np.linalg.norm(reference) / np.linalg.norm(beam_line)
+        return np.arccos(cosine)
+
+    def system_change_matrix_2d_rigid_nodes(self):
+        """
+
+        :return: System change matrix
+        """
+        # Angle must be in radians
+        angle = self._angle_from_global_to_local()
 
         return np.array([
             [math.cos(angle), -math.sin(angle), 0],
@@ -120,9 +139,29 @@ class Bar:
             [0, 0, 1],
         ])
 
+    def global_rigidity_matrix_2d_rigid_nodes(self):
+        g = self.system_change_matrix_2d_rigid_nodes()
+        l = self.local_rigidity_matrix_2d_rigid_nodes()
+
+        m_aux = g.dot(l[0:3, 0:3])
+        k_ii = m_aux.dot(np.transpose(g))
+
+        m_aux = g.dot(l[0:3, 3:6])
+        k_ij = m_aux.dot(np.transpose(g))
+
+        m_aux = g.dot(l[3:6, 0:3])
+        k_ji = m_aux.dot(np.transpose(g))
+
+        m_aux = g.dot(l[3:6, 3:6])
+        k_jj = m_aux.dot(np.transpose(g))
+
+        top = np.hstack((k_ii, k_ij))
+        bottom = np.hstack((k_ji, k_jj))
+
+        return np.vstack((top, bottom))
+
 
 class Structure:
-    # TODO When creating a structure check that there aren't multiple bars with the same name
     def __init__(self, name, bars):
         if type(name) not in [str]:
             raise TypeError("name must be of type str")
@@ -162,10 +201,11 @@ class Material:
         result = db.execute_read_query(conn, query)
 
         if result:
-            self.generic_name, self.name, self.young_mod, self.rig_mod, self.poisson_coef,\
-                self.thermal_dil_coef, self.density = result[0]
+            self.generic_name, self.name, self.young_mod, self.rig_mod, self.poisson_coef, \
+            self.thermal_dil_coef, self.density = result[0]
         else:
-            raise LookupError("Error in the query: ''" + query + "''. Or maybe the material " + name + " is not defined in the database.")
+            raise LookupError(
+                "Error in the query: ''" + query + "''. Or maybe the material " + name + " is not defined in the database.")
 
 
 class Profile:
@@ -185,7 +225,7 @@ class Profile:
 
         query = """SELECT name, name_number, area, weight, inertia_moment_x, res_mod_x,
         inertia_moment_y, res_mod_y
-        FROM profiles WHERE name = '""" + name + "' AND name_number = " +\
+        FROM profiles WHERE name = '""" + name + "' AND name_number = " + \
                 name_number + ";"
 
         # All parameters must be the same than those of the table in the profile database
